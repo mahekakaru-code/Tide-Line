fb6dd9ea5bd5d46d13aa02bbbb2283c596433bdd
mahekakaru-code <maheka.karu@gmail.com>
2026-08-08 20:18:29 +1000

    Fix formatting issues in firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,6 +5,14 @@
    See README.md for step-by-step setup instructions.
    ============================================================ */
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 /* ============================================================
    FIREBASE-CONFIG.JS
    ============================================================ */
